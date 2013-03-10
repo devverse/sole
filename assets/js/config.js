88c9bce509081a28e0e8725ff86991ad19f5c2e6
=Rob <Rob@.(none)>
2013-03-09 21:04:19 -0500

    Updated config and create account

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,5 +3,5 @@ var url = window.location.href;
 if (url.indexOf("192.168.1.2") != -1) {
 	var serviceURL = "http://192.168.1.2/restock/site/public/mobile/";
 } else{
-	var serviceURL = "http://tweency.com/index.php/mobile/";
+	var serviceURL = "http://soleinsider.com/public/mobile/";
 }
