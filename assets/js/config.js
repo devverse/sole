8be0815faf5e6623c25298e6ead09ff168f3af8a
=Rob <Rob@.(none)>
2013-03-14 00:33:04 -0400

    Updated dashboard text and files

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 var url = window.location.href;
 
-if (url.indexOf("192.168.1.2") != -1) {
-	var serviceURL = "http://192.168.1.2/restock/site/public/mobile/";
+if (url.indexOf("localhost") != -1) {
+	var serviceURL = "http://localhost/restock/site/public/mobile/";
 } else{
 	var serviceURL = "http://soleinsider.com/public/mobile/";
 }
